209d3972ca833a6fe1b9155b3772931f1cdf74c3
artemslt <101586284+artemslt@users.noreply.github.com>
2022-08-22 15:42:50 +0000

    Deploying to gh-pages from @ artemslt/goit-js-hw-10@693e9e1ba58aa413cf061a4d7fff42b4bea9c0da 🚀

--- index.8f60a608.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/can").then((t=>t.json())).then((t=>{t.length>10&&alert("ddd"),console.log("data",t)}));
-//# sourceMappingURL=index.8f60a608.js.map
--- index.c4a76462.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/can").then((function(n){return n.json()})).then((function(n){n.length>10&&alert("ddd"),console.log("data",n)}));
-//# sourceMappingURL=index.c4a76462.js.map
